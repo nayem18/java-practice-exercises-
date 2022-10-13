f42415d77cd8bea0398c4c0bce0146f1658eee98
Md. Jannatun Nayem -Git <jannatunnayem18@gmail.com>
2022-10-13 19:39:00 +0600

    commit

---
+++ practice/MultiplyTowNumbers3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MultiplyTowNumbers3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number1: ");
+        int number1 = sc.nextInt();
+
+        System.out.print("Enter the number2: ");
+        int number2 = sc.nextInt();
+        sc.close();
+
+        System.out.println("Product of tow numbers: "+number1 +" x "+number2+"="+number1*number2);
+    }
+}
---
+++ practice/SumTowNumbers2.java
@@ -0,0 +1,8 @@
+public class SumTowNumbers2 {
+    public static void main(String[] args){
+        int num1 = 6;
+        int num2 = 12;
+        double sum = num1 + num2;
+        System.out.println("Tow number of sum: " + sum);
+    }
+}
